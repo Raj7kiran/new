86be3b6c56815b56815f6befe84af9d9286a4e85
rajkiran <raj7kiran@gmail.com>
2020-11-30 06:09:26 +0000

    Added one

---
+++ fakepp.js
@@ -0,0 +1,6 @@
+var faker = require('faker');
+
+var randomName = faker.address.zipCode(); // Rowan Nikolaus
+var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+console.log(randomName);
